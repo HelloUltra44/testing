a3a1caf386dba6155c91ac98293893910a01b10c
HelloUltra44 <122900262+HelloUltra44@users.noreply.github.com>
2023-01-27 15:53:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,44 @@
-import os
+#importing all the necessary libraries
+import json
+import requests
 import speedtest
-import telegram
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters)
 
-def test(update, context):
-    st = speedtest.Speedtest()
-    download = st.download()
-    upload = st.upload()
-    ping = st.results.ping
-    update.message.reply_text(f"Download: {download} \nUpload: {upload} \nPing: {ping}")
+# function to test speed
+def speed_test(bot, update):
+    #performing speed test
+    s = speedtest.Speedtest()
+    s.get_best_server()
+    s.download()
+    s.upload()
+    s.results.share()
+    results_dict = s.results.dict()
+    #storing results in variables
+    download = results_dict["download"]
+    upload = results_dict["upload"]
+    ping = results_dict["ping"]
+    #converting results into megabits
+    download_mb = download / 1000000
+    upload_mb = upload / 1000000
+    #output the results
+    response = (f'Your speed test results\n'
+                f'Download Speed: {download_mb} Mbps\n'
+                f'Upload Speed: {upload_mb} Mbps\n'
+                f'Ping: {ping} ms\n')
+    #send response to the user
+    bot.send_message(chat_id=update.message.chat_id, text=response)
 
-# token from botfather
-TOKEN="5961186050:AAFLW57la19tvwiwVfHlLKoVWFk_ng0uNj0"
+# main function
+def main():
+    #creating telegram bot
+    updater = Updater(token = '5961186050:AAFLW57la19tvwiwVfHlLKoVWFk_ng0uNj0')
+    dispatcher = updater.dispatcher
+    #adding handler
+    dispatcher.add_handler(CommandHandler('speedtest', speed_test))
+    #start the bot
+    updater.start_polling()
+    #run the bot until you press Ctrl-C
+    updater.idle()
 
-updater = Updater(token=TOKEN, use_context=True)
-
-dispatcher = updater.dispatcher
-
-start_handler = CommandHandler('test', test)
-dispatcher.add_handler(start_handler)
-
-updater.start_polling()
+if __name__ == '__main__':
+    main()
