ed1599da2494dd6a19a62bdfa0abee9c50e17af2
HelloUltra44 <122900262+HelloUltra44@users.noreply.github.com>
2023-01-27 13:09:54 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import telebot
+import speedtest
+
+token = 'your_bot_token'
+
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Hi! I can test your server speed. Just type /test')
+
+@bot.message_handler(commands=['test'])
+def test_message(message):
+    st = speedtest.Speedtest()
+    st.get_best_server()
+    st.download()
+    st.upload()
+    res = st.results.dict()
+    bot.send_message(message.chat.id, 'Download speed: {} \nUpload speed: {}'.format(res['download'], res['upload']))
+
+bot.polling()
