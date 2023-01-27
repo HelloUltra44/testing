c090b4eb0bee0a9dcb4ae73bd0b9307d45040fba
HelloUltra44 <122900262+HelloUltra44@users.noreply.github.com>
2023-01-27 13:35:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,63 +1,27 @@
-#importing the necessary libraries
-import requests
-import json
+import speedtest
+import telegram
 import time
-import urllib
 
-#setting the token for the bot
-TOKEN = "5961186050:AAFLW57la19tvwiwVfHlLKoVWFk_ng0uNj0"
+bot_token = '5961186050:AAFLW57la19tvwiwVfHlLKoVWFk_ng0uNj0'
+bot = telegram.Bot(token = bot_token)
 
-#defining the url
-URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+def test_speed():
+    s = speedtest.Speedtest()
+    s.get_best_server()
+    s.download()
+    s.upload()
+    res = s.results.dict()
+    download_speed = res["download"]
+    upload_speed = res["upload"]
+    ping_time = res["ping"]
+    return (download_speed, upload_speed, ping_time)
 
-#function to get the latest update
-def get_updates(offset=None):
-    url = URL + "getUpdates?timeout=100"
-    if offset:
-        url += "&offset={}".format(offset)
-    response = requests.get(url)
-    return json.loads(response.content)
-
-#function to get the latest update
-def get_last_update_id(updates):
-    update_ids = []
-    for update in updates["result"]:
-        update_ids.append(int(update["update_id"]))
-    return max(update_ids)
-
-#function to send a message
-def send_message(text, chat_id):
-    url = URL + "sendMessage?chat_id={}&text={}".format(chat_id, text)
-    requests.get(url)
-
-#function to send an image
-def send_image(image_url, chat_id):
-    url = URL + "sendPhoto?chat_id={}&photo={}".format(chat_id, image_url)
-    requests.get(url)
-
-#function to measure the speed of the server
-def measure_server_speed(url, chat_id):
-    start = time.time()
-    resp = urllib.urlopen(url)
-    end = time.time()
-    download_speed = resp.info().getheader('Content-Length')
-    if download_speed is not None:
-        download_speed = float(download_speed) / (end - start) / 1000
-    send_message("The server download speed is {} GB/s".format(download_speed), chat_id)
-    send_image(url, chat_id)
-
-#main function
-def main():
-    last_update_id = None
-    while True:
-        updates = get_updates(last_update_id)
-        if len(updates["result"]) > 0:
-            last_update_id = get_last_update_id(updates) + 1
-            chat_id = updates["result"][-1]["message"]["chat"]["id"]
-            text = updates["result"][-1]["message"]["text"]
-            if "speed" in text:
-                measure_server_speed("http://example.com/image.jpg", chat_id)
-        time.sleep(0.5)
+def send_speed(download_speed, upload_speed, ping_time):
+    message = "Server speed test results: \nDownload speed: {} Mbps\nUpload speed: {} Mbps\nPing time: {} ms".format(download_speed, upload_speed, ping_time)
+    bot.send_message(chat_id = 'Your_chat_id', text = message)
 
 if __name__ == '__main__':
-    main()
+    while True:
+        download_speed, upload_speed, ping_time = test_speed()
+        send_speed(download_speed, upload_speed, ping_time)
+        time.sleep(3600)
