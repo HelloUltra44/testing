30266f6a092660ad1338fd90b5f8a7b08a0a55ab
HelloUltra44 <122900262+HelloUltra44@users.noreply.github.com>
2023-01-27 16:07:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,21 @@
-#importing all the necessary libraries
-import json
-import requests
 import speedtest
-from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters)
+import telegram
+from telegram.ext import Updater, CommandHandler
 
-# function to test speed
-def speed_test(bot, update):
-    #performing speed test
+def start(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Hello! I am here to help you test your server's speed")
+
+def test_speed(bot, update):
     s = speedtest.Speedtest()
     s.get_best_server()
     s.download()
     s.upload()
-    s.results.share()
-    results_dict = s.results.dict()
-    #storing results in variables
-    download = results_dict["download"]
-    upload = results_dict["upload"]
-    ping = results_dict["ping"]
-    #converting results into megabits
-    download_mb = download / 1000000
-    upload_mb = upload / 1000000
-    #output the results
-    response = (f'Your speed test results\n'
-                f'Download Speed: {download_mb} Mbps\n'
-                f'Upload Speed: {upload_mb} Mbps\n'
-                f'Ping: {ping} ms\n')
-    #send response to the user
-    bot.send_message(chat_id=update.message.chat_id, text=response)
+    result = s.results.dict()
+    bot.send_message(chat_id=update.message.chat_id, text=f"Your download speed is {result['download']} and your upload speed is {result['upload']}")
 
-# main function
 def main():
-    #creating telegram bot
-    updater = Updater(token = '5961186050:AAFLW57la19tvwiwVfHlLKoVWFk_ng0uNj0')
-    dispatcher = updater.dispatcher
-    #adding handler
-    dispatcher.add_handler(CommandHandler('speedtest', speed_test))
-    #start the bot
-    updater.start_polling()
-    #run the bot until you press Ctrl-C
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
+    bot = telegram.Bot(token="5961186050:AAFLW57la19tvwiwVfHlLKoVWFk_ng0uNj0")
+    updater = Updater(bot.token)
+    start_handler = CommandHandler('start', start)
+    speed_handler = CommandHandler('test_speed', test_speed)
+    updater.dispatcher.add_handler(start_handler)
